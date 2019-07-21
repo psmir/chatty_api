c341d29a9fb8e3691eb1df0502ae8c34398b0bb0
Peter <psmir777@gmail.com>
2019-07-21 15:16:57 +0300

    Add user authentication

---
+++ app/mutations/user/auth_token.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class User::AuthToken < BaseMutation
+  required do
+    string :email
+    string :password
+  end
+
+  def authorized?
+    true
+  end
+
+  def execute
+    user = User.find_by(email: email)
+    if user.present? && user.authenticate(password)
+      return JsonWebToken.encode(user_id: user.id)
+    else
+      add_error(:base, :not_authenticated, 'Wrong email or password')
+    end
+  end
+end
