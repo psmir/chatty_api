5c378c96c39d0641bfa044c9757db42c2ffa788c
Peter <psmir777@gmail.com>
2019-08-17 19:30:39 +0300

    Add User::SignUp

---
+++ app/mutations/user/sign_up.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class User::SignUp < BaseMutation
+  required do
+    string :email
+    string :password
+    string :password_confirmation
+  end
+
+  def authorized?
+    true
+  end
+
+  def validation
+    add_error(:email, :not_unique, 'Is already taken') if User.find_by(email: email).present?
+    add_error(:password_confirmation, :not_equal, 'Must be equal to Password') unless password == password_confirmation
+  end
+
+  def execute
+    user = User.new(email: email, password: password)
+    add_error(:base, :something_wrong, 'Something went wrong') && return unless user.save
+
+    User::GenerateAuthToken.run(email: email, password: password).result
+  end
+end
